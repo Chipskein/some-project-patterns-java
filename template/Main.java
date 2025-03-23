78edaf226a66bb0577e688c95f1869a708c06a7f
Chipskein <abfn0905@gmail.com>
2025-03-23 20:46:54 -0300

    update template

--- template/Main.java
+++ template/Main.java
@@ -1,21 +1,21 @@
 import exporter.*;
 class Main {
     public static void main(String[] args) {
-        System.err.println("Testando exporters");
+        System.out.println("Testando exporters");
         
-        System.err.println("Testando HTML");
+        System.out.println("Testando HTML");
         var htmlExporter=new HtmlExporter("exemplo.csv",';');
         htmlExporter.export("exemplo.html");
         
-        System.err.println("Testando PlainText");
+        System.out.println("Testando PlainText");
         var textExporter=new TextExporter("exemplo.csv",';');
         textExporter.export("exemplo.txt");
         
-        System.err.println("Testando XML");
+        System.out.println("Testando XML");
         var xmlExporter=new XMLExporter("exemplo.csv",';');
         xmlExporter.export("exemplo.xml");
         
-        System.err.println("Testando SQL");
+        System.out.println("Testando SQL");
         var sqlExporter=new SQLExporter("exemplo.csv",';',"example");
         sqlExporter.export("exemplo.sql");
 
